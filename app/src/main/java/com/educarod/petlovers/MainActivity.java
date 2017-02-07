126623cf5970f9327516663899b871dc451a40d0
Eduardo Camarillo <camarillor.eduardo@gmail.com>
2017-02-07 01:14:30 -0600

    ft_buttons
    Add RaisedButton and FloatingActionButton.

--- app/src/main/java/com/educarod/petlovers/MainActivity.java
+++ app/src/main/java/com/educarod/petlovers/MainActivity.java
@@ -1,7 +1,9 @@
 package com.educarod.petlovers;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        agregarFAB();
+    }
+
+    public void agregarFAB() {
+        FloatingActionButton miFAB = (FloatingActionButton) findViewById(R.id.fabStar);
+        miFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
